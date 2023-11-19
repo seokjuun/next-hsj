d275f3f7bfa31376abc8a2e46087a5642797b630
seokjun Hong <45346977+seokjuun@users.noreply.github.com>
2023-11-19 17:15:39 +0900

    Update Autoslide.tsx

--- src/components/Autoslide.tsx
+++ src/components/Autoslide.tsx
@@ -68,6 +68,27 @@ export default function Autoslide() {
           />
         ))}
       </div>
-    </>
+      <div className="absolute w-[1922px] h-[992px] top-[620px] bg-[#e2e2e2]">
+      <div className="flex flex-col items-start gap-[15px] relative w-[706px] h-[211px] top-[42px] left-[607px]">
+        <p className="relative self-stretch top-[20px] left-[-250px] [font-family:'NIXGONFONTS_V2.0-R',Helvetica] font-normal text-black text-[50px] text-center tracking-[0] leading-[normal]">
+          <span className="[font-family:'NIXGONFONTS_V2.0-R',Helvetica] font-normal text-black text-[38px] tracking-[0]">
+            어려운 독서 게임을 통해
+            <br />
+          </span>
+        </p>
+        <p className="relative self-stretch left-[-250px] [font-family:'NIXGONFONTS_V2.0-R',Helvetica] font-normal text-black text-[50px] text-center tracking-[0] leading-[normal]">
+          <span className=" [font-family:'NIXGONFONTS_V2.0-R',Helvetica] font-normal text-black text-[38px] tracking-[0]">
+            독서에 재미를 붙여보세요
+            <br />
+          </span>
+        </p>
+        <p className="relative self-stretch [font-family:'NIXGONFONTS_V2.0-R',Helvetica] font-normal text-black text-[50px] text-center tracking-[0] leading-[normal]">
+          <span className="[font-family:'NIXGONFONTS_V2.0-R',Helvetica] font-normal text-black text-[50px] tracking-[0]">
+            {""}
+          </span>
+        </p>
+      </div>
+    </div>
+    </div>
   );
 }
